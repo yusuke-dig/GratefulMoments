498684f29ccd329b3e0472949bc2ad82b23643cb
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-04-03 22:15:04 +0900

    first commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by yusukeyokota on 2026/04/03.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GratefulMomentsTests/GratefulMomentsTests.swift
@@ -0,0 +1,12 @@
+import Testing
+@testable import GratefulMoments
+
+struct GratefulMomentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
