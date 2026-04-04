6f0c897b07a91c74a8cc60e64146b11d4d3fb1ce
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-04-04 22:47:38 +0900

    第3節 写真選択ツールを追加する

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
+import PhotosUI
 
 struct MomentEntryView: View {
     @State private var title: String = ""
     @State private var note: String = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
@@ -11,6 +14,32 @@ struct MomentEntryView: View {
             }
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful For")
+        }
+    }
+    
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
         }
     }
     
@@ -27,6 +56,8 @@ struct MomentEntryView: View {
             TextField("Log your small wins", text: $note, axis: .vertical)
                 .multilineTextAlignment(.leading)
                 .lineLimit(5...Int.max)
+            
+            photoPicker
         }
         .padding()
     }
