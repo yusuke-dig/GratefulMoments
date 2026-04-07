e92316e373b37c34fa0f2e52642d37bf6f73889a
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-04-07 18:51:12 +0900

    第3節 カスタムビューコンポーネントを作成する
    Co-authored-by: Yusuke Yokota <yusuke_yokota_ab@mail.toyota.co.jp>

---
+++ GratefulMoments/Custom Views/Hexagon.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct Hexagon<Content: View>: View {
+    private let borderWidth = 2.0
+    var borderColor: Color = .ember
+    var layout: HexagonLayout = .standard
+    var moment: Moment? = nil
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        ZStack {
+            if let background = moment?.image {
+                Image(uiImage: background)
+                    .resizable()
+                    .scaledToFill()
+            }
+            
+            content()
+                .frame(width: layout.size, height: layout.size)
+        }
+        .mask {
+            Image(systemName: "hexagon.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: layout.size - borderWidth, height: layout.size - borderWidth)
+        }
+        .background {
+            Image(systemName: "hexagon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: layout.size, height: layout.size)
+                .foregroundColor(borderColor)
+        }
+        .frame(width: layout.size, height: layout.size)
+    }
+}
+
+#Preview {
+    Hexagon(layout: .large, moment: Moment.imageSample) {
+        Text(Moment.imageSample.title)
+            .foregroundStyle(Color.white)
+    }
+    .sampleDataContainer()
+}
---
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum HexagonLayout {
+    case standard
+    case large
+    
+    var size: CGFloat {
+        switch self {
+        case .standard:
+            return 200.0
+        case .large:
+            return 350.0
+        }
+    }
+}
