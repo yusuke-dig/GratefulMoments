b4c394c6b969bf3326ca7822727c6cecc2ecd9a8
YusukeYokota <yusuke_yokota_ab@mail.toyota.co.jp>
2026-04-13 22:57:48 +0900

    第3節 バグを修正するが、新たなバグは追加しない

--- GratefulMoments/Logic/StreakCalculator.swift
+++ GratefulMoments/Logic/StreakCalculator.swift
@@ -18,16 +18,16 @@ struct StreakCalculator {
         var streak = 0
         for daysAgo in daysAgoArray {
             if daysAgo == streak {
-                print("Streak already here. Don't increase the streak.")
                 continue
             } else if daysAgo == streak + 1 {
-                print("A moment exists the day after the current streak")
                 streak += 1
-                print("Increasing streak to \(streak)")
             } else {
-                print("Streak of \(streak) broken with daysAgo \(daysAgo)")
                 break
             }
+        }
+        
+        if daysAgoArray.first == 0 {
+            streak += 1
         }
         
         return streak
--- GratefulMomentsTests/StreakCalculatorTests.swift
+++ GratefulMomentsTests/StreakCalculatorTests.swift
@@ -5,10 +5,34 @@ import Foundation
 struct StreakCalculatorTests {
     let streakCalculator = StreakCalculator()
     let now = Date.now
+    
+    struct Input {
+        let expectedStreak: Int
+        let days: [Int]
+    }
 
-    @Test func testCalculations() {
-        let days = [-2, -1]
-        let moments = days.map {
+    
+    @Test("Streak calculations", arguments: [
+        Input(expectedStreak: 0, days: []),
+
+
+        Input(expectedStreak: 1, days: [0]),
+        Input(expectedStreak: 1, days: [-1]),
+        Input(expectedStreak: 0, days: [-2]),
+
+
+        Input(expectedStreak: 1, days: [0, 0]),
+        Input(expectedStreak: 1, days: [-1, -1]),
+        Input(expectedStreak: 0, days: [-2, -2]),
+
+
+        Input(expectedStreak: 3, days: [-2, -1, 0]),
+        Input(expectedStreak: 2, days: [-3, -1, 0]),
+        Input(expectedStreak: 3, days: [-3, -2, -1]),
+        Input(expectedStreak: 2, days: [-4, -2, -1]),
+    ])
+    func testCalculations(input: Input) {
+        let moments = input.days.map {
             let date = Calendar.current.date(byAdding: .day, value: $0, to: now)!
             return Moment(title: "", note: "", timestamp: date)
         }
@@ -17,8 +41,7 @@ struct StreakCalculatorTests {
         let streak = streakCalculator.calculateStreak(for: moments)
         
         //
-        let expectedStreak = 2
-        #expect(streak == expectedStreak)
+        #expect(streak == input.expectedStreak, "\(input.days)")
     }
 
 }
